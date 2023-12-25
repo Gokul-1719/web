4e7849354045c4a52d47e2ef04102ecc8c556bfa
Gokul-1719 <gokulgokul3237@gmail.com>
2023-12-25 10:44:59 +0530

    first commit

---
+++ sginup.php
@@ -0,0 +1,35 @@
+<?php
+// Establish database connection
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "ticket_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Handle form submission
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Hash the password
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    // Insert user data into the database
+    $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$hashed_password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
